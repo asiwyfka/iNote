6fa42e4995aa989474637a21c98bc357473c72f3
Viktor Avdeev <v.avdeev@mail.astondevs.ru>
2025-03-20 01:32:44 +0300

    Initial commit

---
+++ src/main/java/inote/INoteApplication.java
@@ -0,0 +1,13 @@
+package inote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class INoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(INoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inote/INoteApplicationTests.java
@@ -0,0 +1,13 @@
+package inote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class INoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
